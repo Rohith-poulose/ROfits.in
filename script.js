4a8d62ce6e5973bc538e4788f056f5d0a5e5ddbf
Rohith P <rohithinteractive14@gmail.com>
2024-06-16 23:12:56 +0530

    feat: all files updated

---
+++ script.js
@@ -0,0 +1,11 @@
+var pop = document.getElementById("pop");
+var  black = document.getElementById("black");
+
+function update(){
+    pop.style.display="block";
+    black.style.display="block";
+}
+function result(){
+    pop.style.display="none";
+    black.style.display="none";
+}
